e1b17ece5db8a8f5bef96c2ab464fc837b2000fb
Ben-git01 <benbakirbenjamin@orange.fr>
2023-05-31 10:25:30 +0200

    quete 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Pokedex from './components/PokemonCard'
+import PokemonCard from './components/PokemonCard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Pokedex />
+      <PokemonCard />
     </>
   )
 }
