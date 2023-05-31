1a0a30384b669460118a7ee78e122a5bca15c187
Ben-git01 <benbakirbenjamin@orange.fr>
2023-05-31 11:53:38 +0200

    quete 4

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,29 @@ function App() {
 
   return (
     <>
-      <PokemonCard />
+      <PokemonCard name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc} />
     </>
   )
 }
 
 export default App
+
+const pokemonList = [
+
+  {
+
+    name: "bulbasaur",
+
+    imgSrc:
+
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+
+  },
+
+  {
+
+    name: "mew",
+
+  },
+
+];
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,26 +1,12 @@
-const pokemonList = [
 
-    {
 
-        name: "bulbasaur",
+function PokemonCard(props) {
+    console.log(props);
+    let pokemon = {
+        nom: props.name,
+        imgSrc: props.imgSrc
+    };
 
-        imgSrc:
-
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-
-    },
-
-    {
-
-        name: "mew",
-
-    },
-
-];
-
-function PokemonCard() {
-
-    let pokemon = pokemonList[1];
 
 
 
@@ -30,11 +16,11 @@ function PokemonCard() {
 
         <figure>
             {pokemon.imgSrc ? (
-                <img src={pokemon.imgSrc} alt={pokemon.name} />
+                <img src={pokemon.imgSrc} alt={pokemon.nom} />
             )
                 : (<p> ??? </p>)
             }
-            <figcaption>{pokemon.name}</figcaption>
+            <figcaption>{pokemon.nom}</figcaption>
 
         </figure>
     )
